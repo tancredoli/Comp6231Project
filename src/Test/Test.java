418e444efc0267b48cc29300116dec63a2ac50c5
Rui Li <tancredo29@gmail.com>
2019-07-16 15:08:43 -0400

    1st commitment.

---
+++ src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+/**
+ * @Author: Rui
+ * @Description:
+ * @Date: Created in 3:05 PM 2019-07-16
+ * @Modified by:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is an example class");
+    }
+}
