8da1e51d6e5c0267a336a3d0beba5d12949c3be0
yilin0524 <43094334+yilin0524@users.noreply.github.com>
2019-07-16 23:43:39 -0400

    Add files via upload
    Yilin's A2

---
+++ A2Design/DEMSImplementationOfYilin/src/CorbaApp/DEMSInterf.java
@@ -0,0 +1,13 @@
+package CorbaApp;
+
+
+/**
+* CorbaApp/DEMSInterf.java .
+* 由IDL-to-Java 编译器 (可移植), 版本 "3.2"生成
+* 从D:/eclipse-workspace/DEMS1.3/src/corba.idl
+* 2019年7月16日 星期二 上午10时51分31秒 EDT
+*/
+
+public interface DEMSInterf extends DEMSInterfOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity 
+{
+} // interface DEMSInterf
